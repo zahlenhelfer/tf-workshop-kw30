0350a5b7280a3c52736e09bb00acbbf33f9fa19b
Marcus Ross <marcus.ross@zahlenhelfer.de>
2022-07-25 16:18:19 +0200

    added sudo to bootsrap file

--- bootstrap-webserver.sh
+++ bootstrap-webserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-yum install httpd -y
-/sbin/chkconfig --levels 235 httpd on
-service httpd start
-instanceId=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-region=$(curl http://169.254.169.254/latest/meta-data/placement/region)
-echo "<h1>$instanceId from $region</h1>" > /var/www/html/index.html
+sudo yum install httpd -y
+sudo /sbin/chkconfig --levels 235 httpd on
+sudo service httpd start
+sudo instanceId=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+sudo region=$(curl http://169.254.169.254/latest/meta-data/placement/region)
+sudo echo "<h1>$instanceId from $region</h1>" > /var/www/html/index.html
